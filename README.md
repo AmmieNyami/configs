b5ead4cac0aaf491cc366790f4c3cdfd74c166ad
AmmieNyami <ammie@ammienyami.com>
2025-07-09 18:36:33 -0300

    [install.sh] Allow user to choose what gets installed

--- README.md
+++ README.md
@@ -4,8 +4,16 @@ My dotfiles.
 
 ## Installation
 
+To install the configuration for applications, run:
+
 ```console
-$ ./install.sh
+$ ./install.sh common
+```
+
+And to install the configuration for the desktop, run:
+
+```console
+$ ./install.sh common
 ```
 
 The script will tell what commands are missing and need to be installed for the config to work properly.
--- install.sh
+++ install.sh
@@ -1,18 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 . "./install.util.sh"
 
-echo "Installing configs..."
+install_common_configs() {
+    install_config "./emacs"
+    install_config "./shell"
+    install_config "./alacritty"
+}
 
-# Apps
-install_config "./emacs"
-install_config "./shell"
-install_config "./alacritty"
+install_desktop_configs() {
+    install_config "./niri"
+    install_config "./fuzzel"
+    install_config "./dunst"
+    install_config "./waybar"
+}
 
-# Desktop environment
-install_config "./niri"
-install_config "./fuzzel"
-install_config "./dunst"
-install_config "./waybar"
+if [ -z "$1" ]; then
+    echo "ERROR: no subcommand was provided" 1>&2
+    echo "USAGE: install.sh <common | desktop>" 1>&2
+    exit 1
+fi
+
+case "$1" in
+    common)
+        echo "Installing configs..."
+        install_common_configs
+        ;;
+
+    desktop)
+        echo "Installing configs..."
+        install_common_configs
+        install_desktop_configs
+        ;;
+
+    *)
+    echo 'ERROR: unknown subcommand: `'"$1"'`' 1>&2
+    echo "USAGE: install.sh <common | desktop>" 1>&2
+    ;;
+esac
